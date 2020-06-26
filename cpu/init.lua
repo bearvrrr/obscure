e35987a0fcc5dd191ca1bb62faad5370e6ff1167
Andrew Bates <andrew.bates@cantab.net>
2020-06-27 00:50:32 +0100

    CPU widget has label

--- cpu/init.lua
+++ cpu/init.lua
@@ -56,14 +56,24 @@ local function gradient_graph(data)
     return widget
 end
 
-local function create(_)
+local function create(_, label)
+    local label = label or "CPU"
     local data = {}
     data.max = 100
     data.get = function(obj)
         return cpu_usage(obj).perc
     end
 
-    return gradient_graph(data)
+    local widget = wibox.widget {
+        layout = wibox.layout.fixed.horizontal,
+        {
+            text = "CPU",
+            widget = wibox.widget.textbox
+        },
+        gradient_graph(data)
+    }
+
+    return widget
 end
 
 setmetatable(_M, { __call = create })
